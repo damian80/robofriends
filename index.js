d6374ceee10fe8ea07e00402d7141b57de4b3194
Damian Ciasnocha <daneek80@gmail.com>
2019-09-10 22:50:27 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 // import 'tachyons';
 
+
 ReactDOM.render (
-               <div>
-               <Card />
-               <Card />
-               <Card />
-               </div>
+               <App />
 , document.getElementById('root'));
-
-
 serviceWorker.unregister();
